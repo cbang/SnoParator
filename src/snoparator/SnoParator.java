941ca49d2c9a2f52bd2cd7d7fb51f22a2faa558d
cbang13@student.aau.dk <cbang13@student.aau.dk>
2014-11-15 13:56:13 +0100

    Edit of printLn

--- src/snoparator/SnoParator.java
+++ src/snoparator/SnoParator.java
@@ -28,7 +28,7 @@ public class SnoParator extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Much wow");
             }
         });
         
